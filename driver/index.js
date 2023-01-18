66d4a6527aa818ffae61a40539d2d2f76799a36d
stacyyuu <stacyyuu@gmail.com>
2023-01-18 04:09:47 +0000

    lab 11

--- driver/index.js
+++ driver/index.js
@@ -4,6 +4,7 @@ const { handlePickUp } = require('./handler');
 function startDriver() {
   console.log('Driver is ready!');
 
+  // add callback function that's going to be executed when event is triggered
   events.on(EVENT_NAMES.pickup, handlePickUp);
 }
 
